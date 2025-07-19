0025ab38b9575db4880036c4b2a4ad4f999f66de
princeclaude9528 <techmadeus@gmail.com>
2025-07-19 20:59:06 +0100

    fix vite base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // ✅ This makes asset paths relative (required for Vercel)
+  base: "/", 
 });
